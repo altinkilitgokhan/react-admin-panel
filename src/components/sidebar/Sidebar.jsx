497fd5c1c64a4459480b733b569fd66af4496fea
Gokhan ALTINKILIT <gokhan.altinkilit@hotmail.com>
2022-08-07 23:53:57 +0300

    Sidebar updated

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,49 +1,54 @@
 import "./sidebar.scss";
-import DashboardIcon from '@mui/icons-material/Dashboard';
+import DashboardCustomizeOutlinedIcon from '@mui/icons-material/DashboardCustomizeOutlined';
 import LogoutIcon from '@mui/icons-material/Logout';
 import AccountBoxOutlinedIcon from '@mui/icons-material/AccountBoxOutlined';
 import DataUsageIcon from '@mui/icons-material/DataUsage';
-import SettingsIcon from '@mui/icons-material/Settings';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
 import VideoSettingsOutlinedIcon from '@mui/icons-material/VideoSettingsOutlined';
+import AdminPanelSettingsOutlinedIcon from '@mui/icons-material/AdminPanelSettingsOutlined';
 
 const Sidebar = () => {
     return (
         <div className='sidebar'>
             <div className="top">
-                <span className="logo">AdminPanelLogo</span>
+                <AdminPanelSettingsOutlinedIcon className="logo"></AdminPanelSettingsOutlinedIcon>
             </div>
             <hr />
             <div className="center">
                 <ul>
-                    <p className="title">MAIN</p>
                     <li>
-                        <DashboardIcon className="icon"/>
+                        <div className="selectShape"></div>
+                        <DashboardCustomizeOutlinedIcon className="icon"/>
+                        <span>Dashboard</span>
                     </li>
-                    <p className="title">PAGES</p>
                     <li>
+                        <div className="selectShape"></div>
                         <AddCircleOutlineOutlinedIcon className="icon"/>
                         <span>Create New Page</span>
                     </li>
                     <li>
+                        <div className="selectShape"></div>
                         <VideoSettingsOutlinedIcon className="icon"/>
                         <span>Manage Pages</span>
                     </li>
-                    <p className="title">SERVICE</p>
                     <li>
+                        <div className="selectShape"></div>
                         <DataUsageIcon className="icon"/>
                         <span>Logs</span>
                     </li>
                     <li>
-                        <SettingsIcon className="icon"/>
+                        <div className="selectShape"></div>
+                        <SettingsOutlinedIcon className="icon"/>
                         <span>Settings</span>
                     </li>
-                    <p className="title">USER</p>
                     <li>
+                        <div className="selectShape"></div>
                         <AccountBoxOutlinedIcon className="icon"/>
                         <span>Profile</span>
                     </li>
                     <li>
+                        <div className="selectShape"></div>
                         <LogoutIcon className="icon"/>
                         <span>Logout</span>
                     </li>
